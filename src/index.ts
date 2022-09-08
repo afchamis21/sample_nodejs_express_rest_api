fa44f24748dc5d2a3e3875e9c5d9051b75c1b9b8
André Chamis <andrechamis21@gmail.com>
2022-09-08 11:23:18 -0300

    Addded ts and ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import express, { NextFunction, Request, Response } from 'express'
+
+const app = express()
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ message: 'Hello World 2' })
+})
+
+app.listen(5555, () => {
+  console.log('App rodando em http://localhost:5555/')
+})
